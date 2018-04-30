c1e4ed39c48e2b47be79ba9defadbe2688416594
Vinicius Raszl <vini.ra@outlook.com>
2018-04-30 14:07:51 +0100

    jest Test

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
 "use strict";
 exports.__esModule = true;
 var wallet_1 = require("./lib/wallet");
+var asset_1 = require("./lib/asset");
 var PillarSdk = (function () {
     function PillarSdk(configuration) {
         this.wallet = new wallet_1.Wallet();
+        this.asset = new asset_1.Asset();
         PillarSdk.config = configuration;
     }
     PillarSdk.prototype.dumpConfig = function () {
--- dist/utils/signPayload.js
+++ dist/utils/signPayload.js
@@ -1,1 +1,25 @@
+"use strict";
+exports.__esModule = true;
+var SignRequest = (function () {
+    function SignRequest() {
+    }
+    SignRequest.prototype.register = function (walletCreationParams, privateKey) {
+        if (walletCreationParams === null || walletCreationParams === undefined) {
+            throw new Error('Required parameter body was null or undefined when calling createWallet.');
+        }
+        var xAPISignature = auth.sign(walletCreationParams, privateKey, { curve: 'secp256k1' });
+        if (xAPISignature === null || xAPISignature === undefined) {
+            throw new Error('Required parameter xAPISignature was null or undefined when calling createWallet.');
+        }
+        return Requester.register(xAPISignature, walletCreationParams);
+    };
+    SignRequest.prototype.dumpConfig = function () {
+        console.log(this);
+    };
+    SignRequest.prototype.testFunction = function () {
+        return 'hello';
+    };
+    return SignRequest;
+}());
+exports.SignRequest = SignRequest;
 //# sourceMappingURL=signPayload.js.map
