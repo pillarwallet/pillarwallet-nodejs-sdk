85a7cd773835807ee70305103153d032a67462fb
Kieran Goodary <kieran@digitalindustria.com>
2018-05-01 16:11:06 +0100

    feature/connection - accidentally removed Requester.sign method. Reinstating

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,14 @@
 exports.__esModule = true;
 var wallet_1 = require("./lib/wallet");
 var asset_1 = require("./lib/asset");
+var connection_1 = require("./lib/connection");
 var user_1 = require("./lib/user");
 var notification_1 = require("./lib/notification");
 var PillarSdk = (function () {
     function PillarSdk(configuration) {
         this.wallet = new wallet_1.Wallet();
         this.asset = new asset_1.Asset();
+        this.connection = new connection_1.Connection();
         this.user = new user_1.User();
         this.notification = new notification_1.Notification();
         PillarSdk.config = configuration;
--- src/utils/requester.ts
+++ src/utils/requester.ts
@@ -2,6 +2,10 @@ import * as request from 'request-promise';
 const auth = require('@pillarwallet/plr-auth-sdk');
 
 export class Requester {
+    static sign(SignParams: Object,privateKey: string) {
+        return auth.sign(SignParams,privateKey);
+    }
+        
     static execute(incomingRequestOptions: any) {
         return request(incomingRequestOptions);
     }
