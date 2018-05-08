6330acc7595d75e1bf3a715510f6c3537c6e0ee8
Kieran Goodary <kieran@digitalindustria.com>
2018-05-08 10:54:29 +0100

    feature/initial-tests - added ts-loader, webpack, webpack-cli to dev dependencies - added new npm script commands, "bulld", "build-production" - modified tslint to exclude dist - added webpack.config.js file - updated circle ci config to build webpack production mode - removed all now-redundant tsc output files from the dist folder - modified connections.js file in the testapp folder to conform to validation requirements

--- dist/lib/constants/errorMessages.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ErrorMessages;
-(function (ErrorMessages) {
-    ErrorMessages["WalletAlreadyRegistered"] = "Sorry, you have already registered a wallet.   Please instantiate a new PillarSDK to register another wallet.";
-    ErrorMessages["MissingOrInvalidData"] = "Required or invalid data missing. Please check and try again.";
-    ErrorMessages["SigningError"] = "There was a problem signing this request. Please check your   credentials and try again.";
-})(ErrorMessages = exports.ErrorMessages || (exports.ErrorMessages = {}));
-//# sourceMappingURL=errorMessages.js.map
