afe38e25e5a90dfe94bdf97ced26b585adb15677
issa-salad2 <issaraheem@gmail.com>
2018-08-23 16:32:33 +0100

    WIP unit test for investment

--- src/tests/unit/investments.spec.ts
+++ src/tests/unit/investments.spec.ts
@@ -1,5 +1,12 @@
-describe('', () => {
-  it('', () => {
+import { PillarSdk } from '../..';
 
-  });
+let pSdk: PillarSdk;
+
+describe('The Investment Class: depositRequest method', () => {
+  it('should fail validations if the walletID is missing', () => {
+    const inputParams = {
+    currency: "GBP",
+    serviceProvider: "Nivaura",
+    };
+    expect(pSdk.investments.depositRequest(inputParams)).toThrowError("");
 });
